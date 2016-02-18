41527e5f77d73c92862d53a954601ec3cff832c2
Adarsh <adarshsanjeev@gmail.com>
2016-02-18 08:44:17 +0530

    Made functions in members

--- core.py
+++ core.py
@@ -29,28 +29,26 @@ class Player6:
                 adjecent[index] = -1
         adjecent = filter(lambda x: x != -1, adjecent)
         return adjecent
+
+    def get_empty_out_of(board, allowed, block):
+        cells = []
+        for val in allowed:
+            row_val = (val//3)*3
+            col_val = (val%3)*3
+            for i in range(row_val, row_val+3):
+                for j in range(col_val, col_val+3):
+                    if board[i][j] == '-':
+                        cells.append((i, j))
+        if cells == []:
+            allowed = []
+            for val in range(0, 9):
+                if block[val] == '-':
+                    allowed.append(val)
+            cells = get_empty_out_of(board, allowed, block)
+        return cells
     
     def move(self, board, block, old_move, flag):
 	blocks_allowed  = self.get_allowed_blocks(old_move, block)
-        cells = get_empty_out_of(board, blocks_allowed, block)
+        cells = self.get_empty_out_of(board, blocks_allowed, block)
 	return cells[random.randrange(len(cells))]
 
-
-def get_empty_out_of(board, allowed, block):
-    cells = []
-    for val in allowed:
-        row_val = (val//3)*3
-        col_val = (val%3)*3
-        for i in range(row_val, row_val+3):
-            for j in range(col_val, col_val+3):
-                if board[i][j] == '-':
-                    cells.append((i, j))
-
-    if cells == []:
-        allowed = []
-        for val in range(0, 9):
-            if block[val] == '-':
-                allowed.append(val)
-        cells = get_empty_out_of(board, allowed, block)
-
-    return cells
--- test.py
+++ test.py
@@ -47,6 +47,57 @@ class Player1:
 		#Choose a move based on some algorithm, here it is a random move.
 		return cells[random.randrange(len(cells))]
 
+class Player1:
+    def __init__(self):
+        pass
+
+    adjecence_dict = {
+        0: [1, 3],
+        1: [0, 2],
+        2: [1, 5],
+        3: [0, 6],
+        4: [4],
+        5: [2, 8],
+        6: [3, 7],
+        7: [6, 8],
+        8: [5, 7],
+    }
+
+    def get_allowed_blocks(self, old_move, block):
+        if old_move == (-1, -1):
+            return [i for i in range(9)]
+
+        coordinate = (old_move[0]%3, old_move[1]%3)
+        new_block = coordinate[0]*3 + coordinate[1]
+        adjecent = self.adjecence_dict[new_block]
+        for index, val in enumerate(adjecent):
+            if block[val] != '-':
+                adjecent[index] = -1
+        adjecent = filter(lambda x: x != -1, adjecent)
+        return adjecent
+
+    def get_empty(self, board, allowed, block):
+        cells = []
+        for val in allowed:
+            row_val = (val//3)*3
+            col_val = (val%3)*3
+            for i in range(row_val, row_val+3):
+                for j in range(col_val, col_val+3):
+                    if board[i][j] == '-':
+                        cells.append((i, j))
+        if cells == []:
+            allowed = []
+            for val in range(0, 9):
+                if block[val] == '-':
+                    allowed.append(val)
+            cells = self.get_empty(board, allowed, block)
+        return cells
+    
+    def move(self, board, block, old_move, flag):
+	blocks_allowed  = self.get_allowed_blocks(old_move, block)
+        cells = self.get_empty(board, blocks_allowed, block)
+	return cells[random.randrange(len(cells))]
+
 class Player2:
 	
 	def __init__(self):
